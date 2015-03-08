ce6c9978eac405b9fca878df31365bd966fbf44b
jg-123 <roguejk@gmail.com>
2015-03-08 05:17:02 -0600

    added transparent background to plots to exactly match reference plots

--- plot4.R
+++ plot4.R
@@ -10,7 +10,9 @@ if (file.exists("household_power_consumption.txt"))
   pow_consump<-read.table("household_power_consumption.txt",header=TRUE,na.strings="?",sep=";")
   pow_consump_subset<-pow_consump[(pow_consump$Date=="1/2/2007" | pow_consump$Date=="2/2/2007" ),]
   pow_consump_subset$DateTime<-strptime(paste(pow_consump_subset$Date,pow_consump_subset$Time),"%d/%m/%Y %H:%M:%S")
-  png(filename = "plot4.png", width = 480, height = 480, units = "px")
+  # technically, if you look at the example png image files on the github repository, they are transparent...so
+  # added that into my plots. The TAs in the forums said either white background or transparent are fine.
+  png(filename = "plot4.png", width = 480, height = 480, units = "px", bg = "transparent")
   par(mfrow = c(2, 2))
   with (pow_consump_subset,{
     plot(DateTime, Global_active_power, ylab="Global Active Power", xlab="", type="l", col="black")
